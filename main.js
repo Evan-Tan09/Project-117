4d43a2d89a1b858290bb6ef0eb7ba6abcdcc248e
Evan-Tan09 <84255090+Evan-Tan09@users.noreply.github.com>
2022-07-12 18:02:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+array_1=["pen","paper","book","bottle"];
+random_no = Math.floor((Math.random()*array_1.length)+1);
+Element_of_array = array_1[random_no];
+sketch=Element_of_array;
+console.log(sketch);
+timer_counter=0;
+timer_check="";
+drawn_sketch="";
+answer_holder="";
+score=0;
